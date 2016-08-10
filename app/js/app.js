9169be14aa92eb5f4424a2956ecee86fd0776c07
priley86 <priley86@gmail.com>
2016-08-10 00:21:14 -0400

    patternfly browserify example

--- app/js/app.js
+++ app/js/app.js
@@ -3,28 +3,49 @@
 require('es5-shim');
 require('es5-sham');
 
-require('jquery');
+//setting globals used by patternfly
+var $ = (window.jQuery);
+window.d3 = require('d3');
+window.c3 = require('c3');
+
 var angular = require('angular');
-require('angular-route');
 
-var app = angular.module('todoApp', [ 'ngRoute' ]);
+//load patternfly (angular-patternfly index.js)
+require('angular-patternfly');
 
-app.constant('VERSION', require('../../package.json').version);
+var app = angular.module('patternfly.charts').controller( 'ChartCtrl', function( $scope ) {
+  $scope.used = 950;
+  $scope.total = 1000;
+  $scope.available =  $scope.total - $scope.used;
 
-require('./service');
-require('./controller');
+  $scope.chartConfig = $().c3ChartDefaults().getDefaultDonutConfig('MHz Used');
+  $scope.chartConfig.data = {
+    type: "donut",
+    columns: [
+      ["Used", $scope.used],
+      ["Available", $scope.total - $scope.used]
+    ],
+    groups: [
+      ["used", "available"]
+    ],
+    order: null
+  };
 
-app.config(function($routeProvider) {
+  $scope.getChart = function (chart) {
+    $scope.chart = chart;
+  }
 
-  $routeProvider.when('/todos', {
-    templateUrl: 'views/todos.html',
-    controller: 'TodoCtrl',
-  })
-  .when('/imprint', {
-    templateUrl: 'views/imprint.html',
-    controller: 'ImprintCtrl',
-  })
-  .otherwise({
-    redirectTo: '/todos',
-  });
+  $scope.focusUsed = function () {
+    $scope.chart.focus("Used");
+  }
+
+  $scope.updateAvailable = function (val) {
+    $scope.available =  $scope.total - $scope.used;
+  }
+
+  $scope.submitform = function (val) {
+    $scope.used = val;
+    $scope.updateAvailable();
+    $scope.chartConfig.data.columns = [["Used",$scope.used],["Available",$scope.available]];
+  };
 });
--- app/js/service/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var app = require('angular').module('todoApp');
-
-app.service('ImprintService', require('./imprint'));
-app.service('TodoService', require('./todos'));
